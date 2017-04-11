17f3cc91acf352bff81a86e8d14321aac785cca5
michaelcw02 <michael.cw02@hotmail.com>
2017-04-10 22:58:15 -0600

    advances on holiday.js

--- js/holiday.js
+++ js/holiday.js
@@ -1,20 +1,17 @@
 
-const http = require("http");
-const fs = require('fs');
-const net = require('net');
+function Holiday(name, date, observed, public ) {
 
-/*
-var port = 12233;
-var pageURL = "https://holidayapi.com/v1/holidays?key=ea131354-c913-4ed4-8bb5-4673250727c2";
+}
 
-var server = http.createServer( (req, res) => {
-    if(req.method === 'GET') {
-        console.log(req.url);
-        res.write('okay');
-        res.end();
+Holiday.prototype = {
+    Holiday: function(name, date, observed, public) {
+        this.name = name;
+        this.date = date;
+        this.observed = observed;
+        this.public = public;
+    },
+    reviver: function() {
+
     }
-}) 
-server.listen(port, '127.0.0.1');
 
-console.log("Server running at 127.0.0.1:12233");
-*/
+}
